2e7ba68fa739f7a2ad05f99473a9bf8f2208c9c2
Xiaofang <jiangx6@nih.gov>
2025-12-29 13:40:16 -0500

    Sort input sequences by protein length (short to long)
    This improves batch efficiency by grouping similar-length sequences together,
    reducing padding overhead and speeding up inference.

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -17,11 +17,15 @@ class TsvDataLoader(BaseDataLoader):
         if prediction:
             dataset = dataset[["ID", "aa_seq"]] # select columns
             dataset = [ [[(i[0], i[1]), len(i[1])], 0. ] for i in dataset.values]
+            # Sort by protein length (short to long)
+            dataset = sorted(dataset, key=lambda x: x[0][1])
             self.dataset = dataset
             super().__init__(self.dataset, batch_size, shuffle, 0.0, num_workers)
 
         else:
             dataset = dataset[["ID", "aa_seq", "label"]] # select columns
             dataset = [ [[(i[0], i[1]), len(i[1])], i[2] ] for i in dataset.values]
+            # Sort by protein length (short to long)
+            dataset = sorted(dataset, key=lambda x: x[0][1])
             self.dataset = dataset
             super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
